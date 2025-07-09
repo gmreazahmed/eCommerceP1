577c16d603563b48d039e8b7e335a3912bbdaeb4
gmreazahmed <gmreazahmed@gmail.com>
2025-07-10 01:37:25 +0600

    R7

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
 import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/Router';
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-        
+      <RouterProvider router={router}/>
     </>
   )
 }
